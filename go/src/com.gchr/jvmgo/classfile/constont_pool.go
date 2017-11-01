18560f57b64aa8c3fe75364bf5e123e9a8c3bac1
gongchunru <gongchunru@163.com>
2017-11-02 00:25:51 +0800

    add

--- go/src/com.gchr/jvmgo/classfile/constont_pool.go
+++ go/src/com.gchr/jvmgo/classfile/constont_pool.go
@@ -1,3 +1,52 @@
 package classfile
 
+import "fmt"
+
 type ConstantPool []ConstantInfo
+
+//读取常量池
+func readConstantPool(reader *ClassReader) ConstantPool  {
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantInfo, cpCount)
+
+	for i :=1;  i < cpCount; i++  {
+		cp[i] = readConstantPool(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo:
+			i++ //占两个位置
+		}
+	}
+	return cp
+}
+
+//通过索引读取常量结构体
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo{
+	/*
+		为什么不是index - 1 ?
+		因为常量池的索引时从1 开始的
+	 */
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index!")
+	//panic(fmt.Errorf("Invalid constant pool index: %v!", index))
+
+}
+// 读取ConstantNameAndTypeInfo中name_index和descriptor_index对应的字面量
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	//通过索引读取ConstantNameAndTypeInfo
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name,_type
+
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	//通过索引读取CONSTANT_Class_info
+}
+
+// 通过索引获取ConstantUtf8Info结构体的字面值
+func (self ConstantPool) getUtf8(index uint16)  string {
+
+}
--- go/src/com.gchr/jvmgo/classfile/member_info.go
+++ go/src/com.gchr/jvmgo/classfile/member_info.go
@@ -7,3 +7,37 @@ type MemberInfo struct {
 	descriptorIndex uint16
 	attributes      []AttributeInfo
 }
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo  {
+	memberCount := reader.readUint16()
+	members := make([]*MemberInfo, memberCount)
+	for i := range members  {
+		members[i] = readMember(reader, cp)
+	}
+
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo  {
+
+	return &MemberInfo{
+		cp: cp,
+		accessFlags:reader.readUint16(),
+		nameIndex:reader.readUint16(),
+		descriptorIndex:reader.readUint16(),
+		attributes:readAttributes(reader, cp), // 3.4
+	}
+
+}
+
+func (self *MemberInfo) AccessFlags() uint16{
+
+}
+
+func (self *MemberInfo) name() string  {
+
+}
+
+func (self *MemberInfo) Descriptor() string {
+
+}
