68923b47c70b0bf4550ad05c9fa0b63e856129fc
gongchunru <gongchunru@163.com>
2017-12-17 23:34:35 +0800

    .

--- go/src/com.gchr/jvmgo/instructions/constants/ipush.go
+++ go/src/com.gchr/jvmgo/instructions/constants/ipush.go
@@ -1,6 +1,9 @@
 package constants
 
-import "com.gchr/jvmgo/instructions/base"
+import (
+	"com.gchr/jvmgo/instructions/base"
+	"com.gchr/jvmgo/rtdata"
+)
 
 // push byte
 type BIPUSH struct {
@@ -14,6 +17,11 @@ type SIPUSH struct {
 
 func (self *BIPUSH) FetchOperands(reader *base.BytecodeReader)  {
 	self.val = reader.ReadInt8()
+}
+
+func (self *SIPUSH) Execute(frame *rtdata.Frame)  {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
 }
 
 
