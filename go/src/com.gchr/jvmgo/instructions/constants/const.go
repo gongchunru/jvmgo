eac44b97e53ad82994155d45ed8b54879749a816
gongchunru <gongchunru@163.com>
2017-12-18 18:29:34 +0800

    .

--- go/src/com.gchr/jvmgo/instructions/constants/const.go
+++ go/src/com.gchr/jvmgo/instructions/constants/const.go
@@ -10,11 +10,48 @@ type ACONST_NULL struct {
 }
 
 type DCONST_0 struct{ base.NoOperandsInstruction }
+
+// dconst_0 把double类型的0推入操作数栈
+func (self *DCONST_0) Execute(frame *rtdata.Frame){
+	frame.OperandStack().PushDouble(0.0)
+}
+
+// dconst_1 把double类型的1 推入操作数栈
 type DCONST_1 struct{ base.NoOperandsInstruction }
+
+func (self *DCONST_1) Execute(frame *rtdata.Frame)  {
+	frame.OperandStack().PushDouble(1.0)
+}
+
+// 把float类型的0值推入操作数栈
 type FCONST_0 struct{ base.NoOperandsInstruction }
+
+func (self *FCONST_0) Execute(frame *rtdata.Frame)  {
+	frame.OperandStack().PushFloat(0.0)
+}
+
+// 把float类型的1推入操作数栈
 type FCONST_1 struct{ base.NoOperandsInstruction }
+
+func (self *FCONST_1) Execute(frame *rtdata.Frame)  {
+	frame.OperandStack().PushFloat(1.0)
+}
+
+
+// 把float类型的1推入操作数栈
 type FCONST_2 struct{ base.NoOperandsInstruction }
+
+func (self *FCONST_2) Execute(frame *rtdata.Frame)  {
+	frame.OperandStack().PushFloat(1.0)
+}
+
+//把int型-1 推入操作数栈
 type ICONST_M1 struct{ base.NoOperandsInstruction }
+
+func (self *ICONST_M1) Execute(frame rtdata.Frame)  {
+	frame.OperandStack().PushInt(-1)
+}
+
 type ICONST_0 struct{ base.NoOperandsInstruction }
 type ICONST_1 struct{ base.NoOperandsInstruction }
 type ICONST_2 struct{ base.NoOperandsInstruction }
@@ -33,8 +70,5 @@ func (self *ACONST_NULL) Execute(frame rtdata.Frame)  {
 func (self *ICONST_0) Execute(frame rtdata.Frame)  {
 	frame.OperandStack().PushDouble(0.0)
 }
-//把int型-1 推入操作数栈
-func (self *ICONST_M1) Execute(frame rtdata.Frame)  {
-	frame.OperandStack().PushInt(-1)
-}
+
 
